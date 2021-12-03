8e00c66c690ab0d1de3046e4d13a8237e55c1469
Benjamin Jones <benjaminfjones@gmail.com>
2021-12-02 23:16:17 -0800

    Refactor day 3 to use Vec<bool> bitvectors and avoid cloning inner vectors

--- src/d3/bin/main.rs
+++ src/d3/bin/main.rs
@@ -13,65 +13,53 @@ pub fn main() {
 pub fn d3_part1(input_file: &str) -> usize {
     let input_bvs = input_to_bitvectors(input_file);
     let nbits = input_bvs[0].len();
-    let mut gamma_rate: usize = 0;
-    let mut eps_rate: usize = 0;
-    for i in 0..nbits {
-        gamma_rate = gamma_rate << 1;
-        eps_rate = eps_rate << 1;
-        if mcb(&input_bvs, i) == 0 {
-            eps_rate += 1;
-        } else {
-            gamma_rate += 1;
-        }
-    }
-    gamma_rate * eps_rate
+    // gamma_rate has the most common bit at each index
+    let gamma_rate: Vec<bool> = (0..nbits).map(|i| mcb(&input_bvs.iter().collect(), i)).collect();
+    // eps_rate is the bit-wise negation of gamma_rate
+    let eps_rate = gamma_rate.iter().map(|b| !b).collect();
+    bv_to_int(&gamma_rate) * bv_to_int(&eps_rate)
 }
 
 pub fn d3_part2(input_file: &str) -> usize {
     let input_bvs = input_to_bitvectors(input_file);
     let oxy_rate = find_bv_by_selector(&input_bvs, |bvs, i| mcb(bvs, i));
-    let co2_rate = find_bv_by_selector(&input_bvs, |bvs, i| 1 - mcb(bvs, i));
-    oxy_rate * co2_rate
+    let co2_rate = find_bv_by_selector(&input_bvs, |bvs, i| !mcb(bvs, i));
+    bv_to_int(&oxy_rate) * bv_to_int(&co2_rate)
 }
 
 /// parse an input file consisting of one bitstring per line into a vector of bitvectors
-pub fn input_to_bitvectors(input_file: &str) -> Vec<Vec<usize>> {
+pub fn input_to_bitvectors(input_file: &str) -> Vec<Vec<bool>> {
     let content = util::read_to_string(input_file).unwrap();
     let bitstrs: Vec<&str> = content.trim().split('\n').collect();
     bitstrs.iter()
-        .map(|&s| s.chars().map(|c| c.to_digit(2).unwrap() as usize).collect::<Vec<usize>>())
+        .map(|&s| s.chars().map(|c| c == '1').collect::<Vec<bool>>())
         .collect()
 }
 
-/// Return the int value of the first bitvector after repreatedly filtering using the given column
-/// selector
-pub fn find_bv_by_selector<F>(bvs: &Vec<Vec<usize>>, selector: F) -> usize
-    where F: Fn(&Vec<Vec<usize>>, usize) -> usize {
+/// Return the first bitvector after repreatedly filtering using the given column
+/// selector; avoids copying inner bitvectors
+pub fn find_bv_by_selector<F>(bvs: &Vec<Vec<bool>>, selector: F) -> &Vec<bool>
+    where F: Fn(&Vec<&Vec<bool>>, usize) -> bool {
     let nbits = bvs[0].len();
-    let mut bvs_clone = bvs.clone();
+    let mut bv_ptrs: Vec<&Vec<bool>> = bvs.iter().collect();
     for i in 0..nbits {
-        let b = selector(&bvs_clone, i);
-        bvs_clone = bvs_clone.into_iter().filter(|v| v[i] == b).collect();
-        if bvs_clone.len() == 1 {
+        let b = selector(&bv_ptrs, i);
+        bv_ptrs = bv_ptrs.into_iter().filter(|&v| v[i] == b).collect();
+        if bv_ptrs.len() == 1 {
             break;
         }
     }
-    bv_to_int(&bvs_clone[0])
+    bv_ptrs[0]
 }
 
 /// Convert bitvector to unsigned int
-pub fn bv_to_int(bv: &Vec<usize>) -> usize {
-    bv.iter().fold(0, |acc, &x| (acc << 1) + x)
+pub fn bv_to_int(bv: &Vec<bool>) -> usize {
+    bv.iter().fold(0, |acc, &x| (acc << 1) + (if x {1} else {0}))
 }
 
 /// Most common bit at given index
-pub fn mcb(bvs: &Vec<Vec<usize>>, index: usize) -> usize {
-    let col_sum: usize = bvs.iter().map(|v| v[index]).sum();
-    if 2 * col_sum >= bvs.len() {
-        1
-    } else {
-        0
-    }
+pub fn mcb(bvs: &Vec<&Vec<bool>>, index: usize) -> bool {
+    2 * bvs.iter().filter(|&v| v[index]).count() >= bvs.len()
 }
 
 #[cfg(test)]
