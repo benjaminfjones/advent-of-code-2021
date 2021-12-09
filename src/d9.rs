34b3f676a9fba9bbc4e476baf50eb06202fd1a35
Benjamin Jones <benjaminfjones@gmail.com>
2021-12-09 13:48:02 -0800

    Day 9, part 1 solution

---
+++ src/d9.rs
@@ -0,0 +1,77 @@
+/// AoC 2021 -- Day 9
+/// https://adventofcode.com/2021/day/9
+
+use crate::{grid::Grid, util};
+
+#[allow(dead_code)]
+pub struct Window {
+    row: usize, // row, col currently unused
+    col: usize,
+    value: u32,
+    neighbors: Vec<u32>,
+}
+
+pub fn windows(grid: &Grid<u32>) -> Vec<Window> {
+    let mut result: Vec<Window> = Vec::new();
+    for row in 0..grid.rows {
+        for col in 0..grid.cols {
+            let nbs = vec![
+                if row > 0 {grid.get(row-1, col)} else {None},
+                grid.get(row+1, col),
+                if col > 0 {grid.get(row, col-1)} else {None},
+                grid.get(row, col+1),
+            ].into_iter().flatten().map(|v| *v).collect();
+            result.push(Window {
+                row,
+                col,
+                value: *grid.get(row, col).unwrap(),
+                neighbors: nbs,
+            });
+        }
+    }
+    result
+}
+
+pub fn parse_input(input_file: &str) -> Grid<u32> {
+    let content = util::read_to_string(input_file).unwrap();
+    let rows: Vec<Vec<u32>> = content.trim().split('\n')
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect();
+    Grid::from_rows(rows).expect("failed to build grid")
+}
+
+/// returns the sum of low point risk scores
+pub fn d9_part1(grid: &Grid<u32>) -> u32 {
+    let windows = windows(grid);
+    let low_point_windows: Vec<Window> = windows.into_iter()
+        .filter(|w| w.neighbors.iter().all(|&nv| nv > w.value)).collect();
+    low_point_windows.into_iter().map(|w| 1 + w.value).sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parser() {
+        let test_grid = parse_input("inputs/d9_test");
+        assert_eq!(test_grid.rows, 5);
+        assert_eq!(test_grid.cols, 10);
+
+        let grid = parse_input("inputs/d9");
+        assert_eq!(grid.rows, 100);
+        assert_eq!(grid.cols, 100);
+    }
+
+    #[test]
+    fn test_d9_part1_test() {
+        let test_grid = parse_input("inputs/d9_test");
+        assert_eq!(d9_part1(&test_grid), 15);
+    }
+
+    #[test]
+    fn test_d9_part1() {
+        let grid = parse_input("inputs/d9");
+        assert_eq!(d9_part1(&grid), 588);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod d7;
 pub mod d8;
+pub mod d9;
 
 pub mod util {
     use std::fs::File;
@@ -36,6 +37,85 @@ pub mod util {
         match ints_res {
             Ok(v) => Ok(v),
             Err(_) => Err(Error::new(ErrorKind::Other, "i64 parse error"))
+        }
+    }
+}
+
+mod grid {
+    pub struct Grid<T> {
+        pub rows: usize,
+        pub cols: usize,
+        content: Vec<T>,
+    }
+
+    impl<T> Grid<T> {
+        pub fn new(rows: usize, cols: usize, fill: T) -> Self
+            where T: Clone {
+            let mut content =Vec::new();
+            content.resize(rows*cols, fill);
+            Grid {rows, cols, content}
+        }
+
+        pub fn from_vec(rows: usize, cols: usize, content: Vec<T>) -> Result<Self, &'static str> {
+            if content.len() == rows * cols {
+                Ok(Grid {rows, cols, content})
+            } else {
+                Err("rows * cols vs. content length mismatch")
+            }
+        }
+
+        pub fn from_rows(content: Vec<Vec<T>>) -> Result<Self, &'static str> {
+            if content.len() == 0 {
+                return Ok(Grid{rows: 0, cols: 0, content: Vec::new()});
+            }
+            let rows = content.len();
+            let cols = content[0].len();
+            Ok(Grid {rows, cols, content: content.into_iter().flatten().collect()})
+        }
+
+        /// Get element from the grid at (row, col)
+        pub fn get(&self, row: usize, col: usize) -> Option<&T> {
+            if row < self.rows && col < self.cols {
+                Some(&self.content[row*self.cols + col])
+            } else {
+                None
+            }
+        }
+
+        /// Set element on the grid at (row, col)
+        pub fn set(&mut self, row: usize, col: usize, value: T) -> Result<(), ()> {
+            if row < self.rows && col < self.cols {
+                self.content[row*self.cols + col] = value;
+                Ok(())
+            } else {
+                Err(())
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test_util {
+        use super::*;
+
+        #[test]
+        fn test_grid_api() {
+            let mut grid = Grid::new(5, 5, false);
+            assert_eq!(grid.get(0, 0), Some(&false));
+            assert_eq!(grid.get(0,501), None);
+            assert_eq!(grid.get(501, 0), None);
+
+            assert!(grid.set(1, 1, true).is_ok());
+            assert_eq!(grid.get(1, 1), Some(&true));
+        }
+
+        #[test]
+        fn test_from_rows() {
+            let content = vec![
+                vec![0,1,0],
+                vec![2,3,4],
+            ];
+            let grid = Grid::from_rows(content).unwrap();
+            assert_eq!(grid.get(0, 0), Some(&0));
         }
     }
 }
